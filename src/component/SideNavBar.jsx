8100c327e3be7c2cfc1b6a13344a615cedaa1ea1
권기창 <kichang.kwon@aist.co.kr>
2023-04-11 01:48:32 +0900

    edit SideNavBar using chatGPT

--- src/component/SideNavBar.jsx
+++ src/component/SideNavBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const SideNavBar = () => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
 
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
@@ -9,24 +9,57 @@ const SideNavBar = () => {
 
   return (
     <div className="sidenav">
-      <div className="sidenav-header">
-        <h2>My App</h2>
-      </div>
-      <div className="sidenav-menu">
-        <ul>
-          <li>Dashboard</li>
-          <li>
-            <span>Menu 1</span>
-            <span onClick={toggleCollapsed}>{collapsed ? '-' : '+'}</span>
-            {collapsed && <ul className={collapsed ? 'collapsed' : ''}>
-              <li>Submenu 1</li>
-              <li>Submenu 2</li>
-              <li>Submenu 3</li>
-            </ul>}
-          </li>
-          <li>Menu 2</li>
-          <li>Menu 3</li>
-        </ul>
+      <div
+        className={`${
+          collapsed ? 'block' : 'hidden'
+        } md:block md:w-64 md:h-screen md:bg-gray-800 md:fixed md:left-0 md:top-0`}
+      >
+        <div className="flex justify-between items-center md:h-16 md:px-4">
+          <h1 className="text-white font-bold text-xl">My Sidebar</h1>
+        </div>
+        <nav className="mt-2 px-4 md:px-0">
+          <ul className="mt-4 md:mt-0">
+            <li>
+              <a
+                href="#"
+                className="block py-2 text-white font-bold hover:text-gray-500"
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 text-white font-bold hover:text-gray-500"
+              >
+                Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 text-white font-bold hover:text-gray-500"
+              >
+                <span>Menu 1</span>
+                <span onClick={toggleCollapsed}>{collapsed ? '-' : '+'}</span>
+              </a>
+              {collapsed &&
+              <ul className={collapsed ? 'collapsed' : ''}>
+                <li className="block py-2 text-white font-bold hover:text-gray-500">Submenu 1</li>
+                <li className="block py-2 text-white font-bold hover:text-gray-500">Submenu 2</li>
+                <li className="block py-2 text-white font-bold hover:text-gray-500">Submenu 3</li>
+              </ul>}
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 text-white font-bold hover:text-gray-500"
+              >
+                Settings
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
